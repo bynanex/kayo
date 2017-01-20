24f0cdc5b94011bd3dddba50c0f183f726af51c6
aixxe <me@aixxe.net>
2017-01-20 18:47:57 +0000

    Add initial application stylesheet.
    Signed-off-by: aixxe <me@aixxe.net>

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ require('laravel-elixir-vue-2');
  */
 
 elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.less('app.less');
+    mix.styles('bootstrap.css');
+
+    mix.version(['css/app.css', 'css/bootstrap.css']);
 });

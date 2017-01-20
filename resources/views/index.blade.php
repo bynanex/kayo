ba2977dc2835d6fcee76a95ede7999aee2bece76
aixxe <me@aixxe.net>
2017-01-20 18:48:24 +0000

    Add initial index view.
    Signed-off-by: aixxe <me@aixxe.net>

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>kayo</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="{{ elixir('css/bootstrap.css') }}" rel="stylesheet" type="text/css">
+		<link href="{{ elixir('css/app.css') }}" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="container">
+			<header>kayo</header>
+			<main>
+				Content goes here.
+			</main>
+			<footer>
+				<p class="float-left">
+					&copy; 2017, aixxe.net
+				</p>
+				<div class="float-right">
+					<ul class="list-inline">
+						<li class="list-inline-item"><a href="#">Blog</a></li>
+						<li class="list-inline-item"><a href="#">About</a></li>
+						<li class="list-inline-item"><a href="#">Contact</a></li>
+					</ul>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>

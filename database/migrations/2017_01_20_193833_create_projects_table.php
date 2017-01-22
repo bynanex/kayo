94b392039374dd53a411d731652247d5dae1fae7
aixxe <me@aixxe.net>
2017-01-22 21:00:29 +0000

    Add 'summary' and 'language' columns to 'projects' table.
    * Gradually adding stuff.
    
    Signed-off-by: aixxe <me@aixxe.net>

--- database/migrations/2017_01_20_193833_create_projects_table.php
+++ database/migrations/2017_01_20_193833_create_projects_table.php
@@ -18,6 +18,8 @@ class CreateProjectsTable extends Migration
             $table->integer('author_id')->unsigned();
             $table->foreign('author_id')->references('id')->on('users');
             $table->string('name')->unique();
+            $table->string('summary', 256)->unique();
+            $table->string('language', 32);
             $table->string('slug', 64)->unique();
             $table->timestamps();
         });

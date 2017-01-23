5d0eb99f361f6caee10a63d75b3334a0644aa16c
aixxe <me@aixxe.net>
2017-01-23 02:58:08 +0000

    Show banner image on index page.
    * Temporary? Will probably have an exclusive 'index' image.
    
    Signed-off-by: aixxe <me@aixxe.net>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,6 +32,12 @@
 			@foreach (App\Project::all() as $project)
 				<div class="card-columns">
 					<div class="card">
+						@if ($project->bannerUrl)
+						<a href="{{ action('ProjectController@view', [$project->slug]) }}">
+							<img class="card-img-top img-fluid" src="{{ $project->bannerUrl }}" alt="{{ $project->name}}">
+						</a>
+						@endif
+
 						<div class="card-block">
 							<h4 class="card-title">
 								<a href="{{ action('ProjectController@view', [$project->slug]) }}">

27a03711e1edd5ff894df48eacfbe7e9a9f2f5c5
aixxe <me@aixxe.net>
2017-01-30 08:58:30 +0000

    Extend from generic template page for main index view.
    Signed-off-by: aixxe <me@aixxe.net>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,78 +1,53 @@
-@include('templates.headerinclude')
-@include('templates.footer')
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		@yield('headerinclude')
-	</head>
-	<body>
-		<div class="container">
-			<header class="index">
-				<div class="logo">
-					<a href="#">
-						{{-- include the logo vector --}}
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 898 300">
-							{!! file_get_contents('../resources/assets/img/logo.svg') !!}
-						</svg>
+@extends('templates.pages.generic')
+
+@section('content')
+<div class="card-columns">
+	@foreach ($projects as $project)
+		<div class="card">
+			@if ($project->bannerUrl)
+			<a href="{{ action('ProjectController@overview', [$project->slug]) }}">
+				<img class="card-img-top img-fluid" src="{{ $project->bannerUrl }}" alt="{{ $project->name}}">
+			</a>
+			@endif
+
+			<div class="card-block">
+				<h4 class="card-title">
+					<a href="{{ action('ProjectController@overview', [$project->slug]) }}">
+						{{ $project->name }}
 					</a>
+				</h4>
+
+				<small class="card-text">
+					<p>{{ $project->summary }}</p>
+				</small>
+
+				<div>
+					<small class="text-muted">
+						<i class="icon-release"></i> {{ $project->releases->count(). ' '.str_plural('release', $project->releases->count()) }}
+					</small>
 				</div>
-			</header>
-			
-			<main>
-				<div class="card-columns">
-				@foreach ($projects as $project)
-					<div class="card">
-						@if ($project->bannerUrl)
-						<a href="{{ action('ProjectController@overview', [$project->slug]) }}">
-							<img class="card-img-top img-fluid" src="{{ $project->bannerUrl }}" alt="{{ $project->name}}">
-						</a>
-						@endif
-
-						<div class="card-block">
-							<h4 class="card-title">
-								<a href="{{ action('ProjectController@overview', [$project->slug]) }}">
-									{{ $project->name }}
-								</a>
-							</h4>
-
-							<small class="card-text">
-								<p>{{ $project->summary }}</p>
-							</small>
-
-							<div>
-								<small class="text-muted">
-									<i class="icon-release"></i> {{ $project->releases->count(). ' '.str_plural('release', $project->releases->count()) }}
-								</small>
-							</div>
-						</div>
-					</div>
-				@endforeach
-				</div>
-
-				@if ($projects->lastPage() > 1)
-				    <nav class="page-navigation">
-				        @if ($projects->currentPage() !== 1)
-				            <a title="Previous page" href="{{ $projects->previousPageUrl() }}">
-				                <span class="icon-chevron-left"></span>
-				            </a>
-				        @endif
-				        
-				        <span class="separator">
-				            PAGE {{ $projects->currentPage() }} OF {{ $projects->lastPage() }}
-				        </span>
-				        
-				        @if ($projects->hasMorePages())
-				            <a title="Next page" href="{{ $projects->nextPageUrl() }}">
-				                <span class="icon-chevron-right"></span>
-				            </a>
-				        @endif
-				    </nav>
-				@endif
-			</main>
-			
-			<footer>
-				@yield('footer')
-			</footer>
+			</div>
 		</div>
-	</body>
-</html>
+	@endforeach
+	</div>
+
+	@if ($projects->lastPage() > 1)
+		<nav class="page-navigation">
+			@if ($projects->currentPage() !== 1)
+				<a title="Previous page" href="{{ $projects->previousPageUrl() }}">
+					<span class="icon-chevron-left"></span>
+				</a>
+			@endif
+			
+			<span class="separator">
+				PAGE {{ $projects->currentPage() }} OF {{ $projects->lastPage() }}
+			</span>
+			
+			@if ($projects->hasMorePages())
+				<a title="Next page" href="{{ $projects->nextPageUrl() }}">
+					<span class="icon-chevron-right"></span>
+				</a>
+			@endif
+		</nav>
+	@endif
+@endsection

afa2716993bc1cb67bb58241d78fa0071cfc0f41
aixxe <me@aixxe.net>
2017-01-31 03:31:45 +0000

    Add generic error pages for 404 and 503 status codes.
    Signed-off-by: aixxe <me@aixxe.net>

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,10 +4,10 @@
 	<section class="error-container">
 		<span class="icon-warning error-icon text-muted"></span>
 
-		<h1>We'll be right back..</h1>
+		<h1>Page not found</h1>
 		
 		<p class="text-muted">
-			The server is currently unable to handle the request due to a temporary overload or scheduled maintenance, which will likely be alleviated after some delay.
+			Couldn't find that page.
 		</p>
 	</section>
 @endsection

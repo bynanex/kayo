f6fdc64669b5d2af73bff8a60160075ca529838c
aixxe <me@aixxe.net>
2017-01-31 02:50:12 +0000

    Show more information in the project listing profile page.
    Signed-off-by: aixxe <me@aixxe.net>

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -53,6 +53,7 @@
 				<a href="#" class="list-inline-item active">Projects</a>
 			</nav>
 
+			{{-- project listing --}}
 			@foreach ($user->maintains()->orderBy('updated_at', 'desc')->get() as $project)
 				<div class="user-projects">
 					<span class="title">
@@ -66,9 +67,33 @@
 					</span>
 
 					<ul class="list-inline metadata">
+						{{-- language --}}
+						@if ($project->language)
 						<li class="list-inline-item">
 							<i class="icon-gear"></i> {{ $project->language }}
 						</li>
+						@endif
+
+						{{-- show 'open source' if repo_url is set --}}
+						@if ($project->repo_url)
+						<li class="list-inline-item">
+							<i class="icon-fork"></i> Open source
+						</li>
+						@endif
+
+						{{-- show 'maintainer' if not original author --}}
+						@if ($project->author != $user)
+						<li class="list-inline-item">
+							<i class="icon-group"></i> Maintainer
+						</li>
+						@endif
+
+						{{-- show release counter if at least one exists --}}
+						@if ($project->releases->count() >= 1)
+						<li class="list-inline-item">
+							<i class="icon-release"></i> {{ $project->releases->count(). ' '.str_plural('release', $project->releases->count()) }}
+						</li>
+						@endif
 					</ul>
 				</div>
 				

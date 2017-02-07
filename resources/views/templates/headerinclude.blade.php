4b8408e3cf9d21d6ac85ae445d27e32b4f4f5445
aixxe <me@aixxe.net>
2017-02-07 13:07:29 +0000

    Allow webpacked resources to be hosted on a different domain.
    * Additionally, these resources can be contained in a folder such
      as 'cdn.aixxe.net/kayo' - this required a change to the webpack
      configuration file. (publicPath changed from '/' to '../')
    
    Signed-off-by: aixxe <me@aixxe.net>

--- resources/views/templates/headerinclude.blade.php
+++ resources/views/templates/headerinclude.blade.php
@@ -4,8 +4,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="theme-color" content="#2B2B2B">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
-	<link rel="icon" href="/img/favicon.png">
+	<link href="{{ env('STATIC_URL').mix('css/app.css') }}" rel="stylesheet" type="text/css">
+	<link rel="icon" href="{{ env('STATIC_URL').'/img/favicon.png' }}">
 	<link rel="apple-touch-icon" href="img/icon.png">
 	<style type="text/css">
 		@stack('styles-inline')
